e9776c0ecb8fea5e02db9076ab3a501381da6db1
WiredDevs <58932649+maxwireddevs@users.noreply.github.com>
2020-06-06 21:39:56 +0800

    Update _keys.js

--- _keys.js
+++ _keys.js
@@ -1,3 +1,3 @@
 
-module.exports.weatherapi = "30469e7bb59d44ba9e0105606200606";
-module.exports.key = "NzE4NzczNzkxOTQ1MjYxMDk3.XttxCQ.V1jOUobGcUQLt6RBqOWDbB8IEVA";
+module.exports.weatherapi = "xxxx";
+module.exports.key = "xxxx";

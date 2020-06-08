4991e99a12256eac5c686a8612126323468ac55c
WiredDevs <58932649+maxwireddevs@users.noreply.github.com>
2020-06-08 18:00:01 +0800

    Update alpha.js

--- alpha.js
+++ alpha.js
@@ -345,7 +345,7 @@ bot.on("message", (message) => {
 				break;	
 
 			case "help":
-				message.channel.send("Available commands: comfy, scared, bored, photo, unpack, bye, coffee, welcome, taste, guitar, ehehe, kanji, weather in <city name>, link");
+				message.channel.send("Available commands: comfy, scared, bored, photo, unpack, bye, coffee, welcome, taste, guitar, ehehe, splash, nuke, kanji, weather in <city name>, link");
 				break;
 
 			case "link":
